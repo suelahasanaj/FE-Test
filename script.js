27a11b241c5618c17915c8d33181023fb15ad34c
suelahasanaj <suelahasanaj6@gmail.com>
2024-04-15 21:24:59 +0200

    Phase 3 - responsive layout

--- script.js
+++ script.js
@@ -2,4 +2,16 @@ new Glide('.glide',{
     type: 'carousel',
     perView:5,
     startAt: 0,
+    breakpoints: {
+        767:{
+            perView: 2
+        },
+        992:{
+            perView: 4
+        },
+        1200:{
+            perView: 5
+        },
+
+    }
 }).mount();
